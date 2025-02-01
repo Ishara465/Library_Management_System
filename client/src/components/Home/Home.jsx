71679328add2b315bdfed401c42cedc5cad54eec
Ishara465 <116372693+Ishara465@users.noreply.github.com>
2025-02-01 11:37:40 +0530

    homepage

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import Form from "react-bootstrap/Form";
+
+
+import { Container } from "react-bootstrap";
+import SideNavigation from '../importentComponents/SideNavigation';
 
 const Home = () => {
   return (
-    <div>
- <p>home</p>
-  
-    </div>
+    <div style={{ display: "flex", height: "100vh" }}>
+    {/* Sidebar */}
+    <SideNavigation />
+    {/* Main Content */}
+    <Container fluid className="p-4">
+      <div className="container p-4">
+        <div
+          className="col-12 p-4 rounded-5 shadow"
+          style={{ backgroundColor: "rgb(221, 222, 223)" }}
+        >
+          <div className="row   d-flex justify-content-center  mt-5">
+             {/* Home Body */}
+              <h1>Home Page</h1>
+           
+
+            
+          </div>
+        </div>
+      </div>
+    </Container>
+  </div>
   )
 }
 
